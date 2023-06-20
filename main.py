efe1e3899b72121119d1b15d36df6ed88290e4e6
Phạm Thành Trung <thanhtrungit2001@gmail.com>
2023-06-20 16:04:08 +0700

    add service crawl lineups

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from services.game_history import GameHistory
+from services.team_info import TeamInfo
 
 if __name__ == '__main__':
     game_history = GameHistory()
     game_history.crawler()
+    
+    team_info = TeamInfo()
+    team_info.crawler()
